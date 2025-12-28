8a479c12a909481db537d555cf6607253a6d1811
Nischal07bot <singhnischal953@gmail.com>
2025-12-29 02:55:55 +0530

    made the worker processing jobs

--- my-turborepo/apps/worker/src/index.ts
+++ my-turborepo/apps/worker/src/index.ts
@@ -3,5 +3,69 @@ import { Worker } from "bullmq";
 import { createRedisConnection } from "@repo/queue";
 import { prisma } from "@repo/db";
 import fetch from "node-fetch";
+import { DeliveryStatus } from "@repo/db";
+
+console.log("Worker started...");
 
 
+const worker = new Worker('deliveryQueue', async(job)=>{
+    const { deliveryId } = job.data;
+    const delivery = await prisma.delivery.findUnique({
+        where:{
+            id: deliveryId
+        },
+        include:{
+            event:true,
+            webhook:true
+        }
+    })
+    if(!delivery){
+        throw new Error("Delivery not found");
+    }
+    try{
+        const response = await fetch(delivery.webhook.url,{
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "X-Event-Type": delivery.event.type
+            },
+            body: JSON.stringify(delivery.event.payload)
+        });
+        await prisma.delivery.update({
+            where:{
+                id: delivery.id
+            },
+            data:{
+                status: response.ok ? DeliveryStatus.SUCCESS : DeliveryStatus.FAILED,
+                responseCode: response.status,
+            }
+        })
+        if(!response.ok){
+            throw new Error(`Failed with status ${response.status}`);
+        }
+    }
+    catch(error:any){
+        await prisma.delivery.update({
+            where:{
+                id: delivery.id
+            },
+            data:{
+                status: DeliveryStatus.RETRYING,
+                error: error.message,
+            }
+        });
+        throw error;
+    }
+
+   },
+   {
+    connection: createRedisConnection
+   }
+
+);
+worker.on('completed',(job)=>{
+    console.log('Job completed:',job.id);
+})
+worker.on('failed',(job,err)=>{
+    console.log(`Job failed: ${job?.id}, error: ${err.message}`);
+});

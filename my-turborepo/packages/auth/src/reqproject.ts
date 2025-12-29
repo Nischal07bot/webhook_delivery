cbffe046f019117bb4eb00d1c82cbed128d65b37
Nischal07bot <singhnischal953@gmail.com>
2025-12-29 15:02:44 +0530

    project auth middleware added in src/auth

---
+++ my-turborepo/packages/auth/src/reqproject.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@repo/db";
+import { NextRequest } from "next/server";
+
+export async function reqproject(request: NextRequest){ 
+    const body=request.headers.get("authorization");
+
+    if(!body || !body.startsWith("Bearer ")){
+        throw new Error("Unauthorized: Missing or invalid authorization header");
+    }
+    const apiKey=body.split(" ")[1];
+
+    const project=await prisma.project.findUnique({
+        where:{
+            apiKey:apiKey
+        }
+    })
+    if(!project){
+        throw new Error("Unauthorized: Invalid API key");
+    }
+    return project;
+}

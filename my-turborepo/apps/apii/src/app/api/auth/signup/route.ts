48779ec6edb02a61609834d393c0d8ead37f7837
Nischal07bot <singhnischal953@gmail.com>
2026-01-02 20:40:40 +0530

    signup endpoint done and cookie set in response

--- my-turborepo/apps/apii/src/app/api/auth/signup/route.ts
+++ my-turborepo/apps/apii/src/app/api/auth/signup/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse,NextRequest } from "next/server";
 import { prisma } from "@repo/db"
-
+import { hashPassword } from "@/src/lib/encodedec";
+import { cookies } from "next/headers";
 export async function POST(request: NextRequest){
     const {email,password} =await request.json();
 
@@ -22,14 +23,27 @@ export async function POST(request: NextRequest){
         );
     }
 
+    const hashedpassword= await hashPassword(password);
     const newUser =await prisma.user.create({
         data:{
             email:email,
-            password:password,
+            password:hashedpassword,
 
         }
     })
 
-    
-    
+    const session=await prisma.session.create({
+        data:{
+            userId:newUser.id,
+            expiresAt:new Date(Date.now()+ 1000 * 60 * 60 * 24 * 30)
+        }
+    })
+    const response = NextResponse.json({ message: "Signup successful" });
+    response.cookies.set("session", String(session.id), {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    });
+    return response;
 }

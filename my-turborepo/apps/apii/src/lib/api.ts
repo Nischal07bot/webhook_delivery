1e02d5514ce26fde2e5fc0c9af11816170f1eccc
Nischal07bot <singhnischal953@gmail.com>
2026-01-05 13:07:57 +0530

    added common fetch for modularity

---
+++ my-turborepo/apps/apii/src/lib/api.ts
@@ -0,0 +1,15 @@
+export async function fetchapi(url: string, options: RequestInit={}): Promise<any> {
+	const response=await fetch(url,{
+        credentials: 'include',
+        headers:{
+            'Content-Type': 'application/json',
+            ...(options?.headers || {})
+        },
+        ...options
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
